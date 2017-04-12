65ac139608e446f86854273939054f56bbc1ca99
unknown <opatou@LFR012144.corp.capgemini.com>
2017-04-12 11:21:02 +0200

    second commit

---
+++ katajeudelavie/src/main/java/com/capgemini/formation/kata/katajeudelavie/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.formation.kata.katajeudelavie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World : That's the Life Game!" );
+    }
+}
---
+++ katajeudelavie/src/test/java/com/capgemini/formation/kata/katajeudelavie/JeuDeLaVieTest.java
@@ -0,0 +1,5 @@
+package com.capgemini.formation.kata.katajeudelavie;
+
+public class JeuDeLaVieTest {
+
+}
